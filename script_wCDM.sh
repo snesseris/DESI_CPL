1910205c940ac064840ac786cfacac19c9500df1
Savvas Nesseris <31890417+snesseris@users.noreply.github.com>
2026-08-31 16:39:33 +0200

    Add files via upload

---
+++ script_wCDM.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#SBATCH -J SPARTA
+#SBATCH --partition=batch4
+#SBATCH --get-user-env
+#SBATCH -o ./job.%j.out
+#SBATCH -e ./job.%j.err
+#SBATCH -n 1
+#SBATCH -t 2-01:59:00
+
+wolframscript ./script_MCMC_wCDM.wls
